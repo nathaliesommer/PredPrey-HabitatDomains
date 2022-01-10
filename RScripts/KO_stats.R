fef58a11f47304e8f6de11783f4f7c2e0bbff512
Kaggie Orrick <kaggieorrick@vpn1722515786.its.yale.internal>
2022-01-10 15:40:46 -0500

    surv stats?

--- RScripts/KO_stats.R
+++ RScripts/KO_stats.R
@@ -37,12 +37,157 @@ ipak(packages)
 
 
 # load data ----
-oneyr <- read_csv("Data/NCvsC_1year_TSH_Nov29.csv")
+oneyr <- read_csv("Data/NCvsC_1year_TSH_Nov29.csv") %>%
+  mutate(ModelType = 1)
 fiveyr <- read_csv("Data/NCvsC_5year_TSH_Nov29.csv")
-oneyr_null <- read_csv("Data/NCvsC_Nullyear_TSH_Nov19.csv")
+oneyr_null <- read_csv("Data/NCvsC_Nullyear_TSH_Nov19.csv") %>%
+  mutate(ModelType = 0)
 
 fiveyr_null <- read_csv("Data/NCvsC_NULL_5year_TSH_Nov29.csv")
 # use df data from NCvsC_5years_Space_Time_Habitat
+
+oneYearNullandTrue <- rbind(oneyr, oneyr_null)
+
+oneYearNullandTrue <- oneYearNullandTrue  %>%
+  mutate(propHabitat = White.Table/(Black.Table + White.Table)) %>%
+  mutate(propSafeSpace = Domain.Prey/(Domain.Prey + DomainOverlap)) %>%
+  mutate(propPredFree = rowSums(.[28:39])/rowSums(.[16:39]))
+  
+testing<- glm(ModelType ~ ticks + propHabitat + propSafeSpace + propPredFree + Pred.Prey_Domain, family = binomial, data = oneYearNullandTrue)
+summary(testing)
+
+# separate predator strategies
+active1 <- subset(oneYearNullandTrue, Pred.Strat == "Active") %>%
+  mutate(Pred.Prey_Domain =interaction(Pred.Start.Con, Prey.Start.Con)) 
+  select( Pred.Prey_Domain, propW, propPredFree, propSafeSpace, ticks)
+
+active1.Small.Small <- subset(oneYearNullandTrue, Pred.Strat == "Active") %>%
+  mutate(Pred.Prey_Domain =interaction(Pred.Start.Con, Prey.Start.Con)) %>%
+  subset(Pred.Prey_Domain == "Small.Small")
+  
+  testing<- glm(ModelType ~ ticks + propHabitat + propSafeSpace + propPredFree, family = binomial, data = active1.Small.Small)
+  summary(testing)
+
+
+###### Now for 5 years
+  
+  # load data ----
+
+  fiveyr <- read_csv("Data/NCvsC_5year_TSH_Nov29.csv")%>%
+    mutate(ModelType = 1)
+
+  fiveyr_null <- read_csv("Data/NCvsC_NULL_5year_TSH_Nov29.csv")  %>%
+    mutate(ModelType = 0)
+
+FiveYearNullandTrue <- rbind(fiveyr, fiveyr_null)
+  
+FiveYearNullandTrue <- FiveYearNullandTrue  %>%
+    mutate(propHabitat = White.Table/(Black.Table + White.Table)) %>%
+    mutate(propSafeSpace = Domain.Prey/(Domain.Prey + DomainOverlap)) %>%
+    mutate(propPredFree = rowSums(.[28:39])/rowSums(.[16:39]))
+  
+  testing<- glm(ModelType ~ ticks + propHabitat + propSafeSpace + propPredFree + Pred.Prey_Domain, family = binomial, data = oneYearNullandTrue)
+  summary(testing)
+  
+  # separate predator strategies
+  active5 <- subset(FiveYearNullandTrue, Pred.Strat == "Active") %>%
+    mutate(Pred.Prey_Domain =interaction(Pred.Start.Con, Prey.Start.Con)) 
+  
+  active5.Small.Small <- subset(FiveYearNullandTrue, Pred.Strat == "Active") %>%
+    mutate(Pred.Prey_Domain =interaction(Pred.Start.Con, Prey.Start.Con)) %>%
+    subset(Pred.Prey_Domain == "Small.Small")
+  
+  active5.Large.Small <-  subset(FiveYearNullandTrue, Pred.Strat == "Active") %>%
+    mutate(Pred.Prey_Domain =interaction(Pred.Start.Con, Prey.Start.Con)) %>%
+    subset(Pred.Prey_Domain == "Large.Small")
+  
+SW5.Large.Small <-  subset(FiveYearNullandTrue, Pred.Strat == "Sit-and-Wait") %>%
+    mutate(Pred.Prey_Domain =interaction(Pred.Start.Con, Prey.Start.Con)) %>%
+    subset(Pred.Prey_Domain == "Large.Small")
+  
+  testing5<- glm(ModelType ~ ticks + propHabitat + propSafeSpace + propPredFree, family = binomial, data = active5.Small.Small)
+  summary(testing5)
+  
+  active5.Large.Small.glm<- glm(ModelType ~ ticks + propHabitat + propSafeSpace + propPredFree, family = binomial, data = active5.Large.Small)
+  summary(active5.Large.Small.glm)
+  
+  SWe5.Large.Small.glm<- glm(ModelType ~ ticks + propHabitat + propSafeSpace + propPredFree, family = binomial, data = SW5.Large.Small)
+  summary(SWe5.Large.Small.glm)
+  
+
+####### 
+  library(survival)
+help(lung)  
+View(lung)
+
+survobj<- with(lung, Surv(time,status))
+fit0 <- survfit(survobj~1, data=lung)
+summary(fit0)
+plot(fit0, xlab="Survival Time in Days", 
+     ylab="% Surviving", yscale=100,
+     main="Survival Distribution (Overall)") 
+
+# Compare the survival distributions of men and women 
+fit1 <- survfit(survobj~sex,data=lung)
+
+# plot the survival distributions by sex 
+plot(fit1, xlab="Survival Time in Days", 
+     ylab="% Surviving", yscale=100, col=c("red","blue"),
+     main="Survival Distributions by Gender") 
+legend("topright", title="Gender", c("Male", "Female"),
+       fill=c("red", "blue"))
+
+# test for difference between male and female 
+# survival curves (logrank test) 
+survdiff(survobj~sex, data=lung) 
+
+# predict male survival from age and medical scores 
+MaleMod <- coxph(survobj~age+ph.ecog+ph.karno+pat.karno,
+                 data=lung, subset=sex==1)
+
+# display results 
+MaleMod
+
+# evaluate the proportional hazards assumption 
+cox.zph(MaleMod)
+
+#3 need status to be 1 or 0 
+
+oneYearNullandTrue_surv <-   active5.Small.Small %>%
+  mutate(status = ifelse (ticks == 43800, 0, 1))
+
+survobj<- with(oneYearNullandTrue_surv, Surv(ticks,status))
+fit0 <- survfit(survobj~1, data=oneYearNullandTrue_surv)
+summary(fit0)
+plot(fit0, xlab="Survival Time in Hours", 
+     ylab="% Surviving", yscale=100,
+     main="Survival Distribution (Overall)") 
+
+# Compare the survival distributions of men and women 
+fit1 <- survfit(survobj~ModelType, data=oneYearNullandTrue_surv)
+
+# plot the survival distributions by sex 
+plot(fit1, xlab="Survival Time in ticks", 
+     ylab="% Surviving", yscale=100, col=c("red","blue"),
+     main="Survival Distributions Null vs NCE") 
+legend("topright", title="Model Type", c("Null", "NCE"),
+       fill=c("red", "blue"))
+
+# test for difference between male and female 
+# survival curves (logrank test) 
+survdiff(survobj~ModelType, data=oneYearNullandTrue_surv) 
+
+# predict male survival from age and medical scores 
+MaleMod <- coxph(survobj~ propHabitat + propSafeSpace + propPredFree,
+                 data=oneYearNullandTrue_surv)
+
+# display results 
+MaleMod
+
+# evaluate the proportional hazards assumption 
+cox.zph(MaleMod)
+
+
 
 # convert to factors ----
 # hunting mode and starting conditions as factors
